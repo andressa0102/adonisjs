55eb46da25fed714813caee4ae5d3f1c43c503a3
andressa0102 <andressas.as1235@gmail.com>
2023-08-14 11:30:03 -0300

    adonis

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -15,7 +15,8 @@ export default class AuthController {
             const token = await auth.use('api').attempt(email, password, {
                 expiresIn: '1day'
             })
-            const user = await User.findOrFail("email", email)
+            console.log('oi')
+            const user = await User.findByOrFail("email", email)
             return { token, user}
         } catch (error) {
             response.status(401).send("Login ou senha incorretos!!!")
