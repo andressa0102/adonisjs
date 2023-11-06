d9d029339ec8f60edb6400057a80532cd3f17207
andressa0102 <andressas.as1235@gmail.com>
2023-11-06 12:19:19 -0300

    fila acabada

--- start/routes.ts
+++ start/routes.ts
@@ -28,5 +28,5 @@ Route.post('/register', 'AuthController.register')
 Route.post('/login', 'AuthController.login')
 Route.group(() => {
   Route.put("/user", "UsersController.update")
-  Route.post("/push", "SenPushesController.send")
+  Route.post("/push", "SendPushesController.send")
 }).middleware('auth')
